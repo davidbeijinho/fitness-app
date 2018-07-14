09a4366b879acd0ed36037727150e942b07cf3ad
davidbeijinho <davidbeijinho@gmail.com>
2018-07-14 02:15:08 +0200

    add constants to utils

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,17 @@
-import CONSTANS from './constants';
+const CONSTANS = {
+    LEVEL_RATIOS: {
+        1: 1.2,
+        2: 1.375,
+        3: 1.55,
+        4: 1.725,
+        5: 1.9,
+    },
+    GOAL: {
+        1: 0,
+        2: 500,
+        3: -500,
+    }
+};;
 
 function calculateMBASAL(data) {
     if (data.sex === 'MALE') {
