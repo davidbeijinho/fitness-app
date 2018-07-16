a01c01c84b7132a5fd22940ee63e5df64af1645b
davidbeijinho <davidbeijinho@gmail.com>
2018-07-16 22:32:42 +0200

    fix styles in input question

--- src/components/InputQuestion.js
+++ src/components/InputQuestion.js
@@ -2,20 +2,21 @@ import React from "react";
 import PropTypes from 'prop-types';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
 import CardQuestion from "./CardQuestion";
+import TextField from '@material-ui/core/TextField';
 
 const InputQuestion = ({ question, value, sendState, enabledLabel, disabledLabel }) => (
   <CardQuestion title={question.title} link={question.link} value={value} enabledLabel={enabledLabel} disabledLabel={disabledLabel}>
     {question.options.map(function (option) {
       return <FormControl key={option.id} fullWidth aria-describedby="weight-helper-text" >
-        <InputLabel key={`label-${option.id}`} htmlFor={option.id}>{option.label}</InputLabel>
-        <Input
+        <TextField
+          InputProps={{
+            endAdornment: <InputAdornment position="end">{option.adornment}</InputAdornment>
+          }}
           id={option.id}
+          label={option.label}
           onChange={sendState[option.onChange]}
-          endAdornment={<InputAdornment position="end">{option.adornment}</InputAdornment>}
-          inputProps={{ 'aria-label': option.label, }}
+          margin="normal"
         />
       </FormControl>;
     })}
