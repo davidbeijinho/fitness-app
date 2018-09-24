6c8eee38b791b4a6de1799ceae448e97a39e82db
davidbeijinho <davidbeijinho@gmail.com>
2018-09-25 00:21:48 +0200

    move files to replicate structure, update api to use new structure ,and add error logo

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 function loadData() {
-  return Promise.all([axios.get('/data/questions.json'), axios.get('/data/results.json'), axios.get('/data/others.json')])
+  return Promise.all([axios.get('/wp-content/data/questions.json'), axios.get('/wp-content/data/results.json'), axios.get('/wp-content/data/others.json')])
     .then(values => ({
       questions: values[0].data,
       results: values[1].data,
       others: values[2].data,
-    }));
+    })).catch((err, ...args) => {
+      console.log(args);
+      console.log('error', err);
+    });
 }
 
 export default {
