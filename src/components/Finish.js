f8355bfb9bd5c4e11590b3522b097c0338aaa963
davidbeijinho <davidbeijinho@gmail.com>
2018-09-25 00:41:14 +0200

    simplify finish, add redirector

--- src/components/Finish.js
+++ src/components/Finish.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import CardHeader from '@material-ui/core/CardHeader';
 import utils from '../utils/utils';
-import Results from './Results';
+import Redirector from './Redirector';
+
 
 const Finish = ({ title, buttonLabel, ...rest }) => (
-  <Card>
-    <CardHeader title={title} />
-    <CardContent>
-      {utils.getText(rest)}
-      <Results {...rest} />
-    </CardContent>
-    <CardActions>
-      <a href={utils.getLink(rest)}>
-        <Button variant="contained" color="primary">
-          {buttonLabel}
-        </Button>
-      </a>
-    </CardActions>
-  </Card>
+  <Redirector link={utils.getLink(rest)} />
 );
 
 Finish.propTypes = {
