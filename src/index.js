ff8d0abb7f3b307cf178cd6f111c86ea64a937c6
davidbeijinho <davidbeijinho@gmail.com>
2018-09-25 00:04:53 +0200

    add theme, update info

--- src/index.js
+++ src/index.js
@@ -4,12 +4,30 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import App from './components/App';
 import store from './store/store';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#80f480',
+    },
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      'Montserrat',
+    ].join(','),
+  },
+});
+
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root-fitness-app'),
 );
